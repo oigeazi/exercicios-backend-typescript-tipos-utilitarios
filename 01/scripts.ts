bdee958692eaca6bbc88591bf53b569a196702fb
Geazi <oigeazi@gmail.com>
2023-08-28 17:56:26 -0300

    Resolvido

--- 01/scripts.ts
+++ 01/scripts.ts
@@ -1,11 +1,14 @@
 type Usuario = {
-    nome: string,
-    email: string,
-    cpf: string,
-    rg: string
-    dataNacimento?: string
-}
+  nome: string;
+  email: string;
+  cpf: string;
+  rg: string;
+  dataNacimento?: string;
+};
+
+type UsuarioSemRg = Omit<Usuario, "rg">;
+type UsuarioCamposObrigatorios = Required<UsuarioSemRg>;
 
 const cadastrarUsuário = (info: Usuario): Usuario => {
-    return info;
-}
+  return info;
+};
--- 02/scripts.ts
+++ 02/scripts.ts
@@ -1,21 +1,39 @@
 type Item = {
-    nome: string,
-    descricao: string,
-    valor: number
-}
+  nome: string;
+  descricao: string;
+  valor: number;
+};
 
 type Cartao = {
-    numero: number,
-    validade: string,
-    nome: string,
-    cvv: number
-}
+  numero: number;
+  validade: string;
+  nome: string;
+  cvv: number;
+};
 
 type Carrinho = {
-    item: Item,
-    qtd: number,
-    desconto: number,
-    frete: number,
-    tipoTransacao: 'credito' | 'Debito',
-    cartao: Cartao
-}
+  item: Item;
+  qtd: number;
+  desconto: number;
+  frete: number;
+  tipoTransacao: "credito" | "Debito";
+  cartao: Cartao;
+};
+
+type Endereco = {
+  cep: string;
+  rua: string;
+  bairro: string;
+  cidade: string;
+  estado: string;
+};
+
+type NovoCarrinho = {
+  item: Item;
+  qtd: number;
+  desconto: number;
+  frete: number;
+  tipoTransacao: Lowercase<"credito" | "Debito">;
+  cartao: Cartao;
+  endereco: Endereco;
+};
